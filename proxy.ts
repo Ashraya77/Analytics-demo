c5050173ca010a6c4fcb47e5dbf51a5171a0fcb1
Ashraya <aashray851@gmail.com>
2026-05-02 16:41:51 +0545

    modified proxy page

--- proxy.ts
+++ proxy.ts
@@ -1,22 +1,32 @@
 import { auth } from "@/auth";
-  import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-  export default auth((req) => {
-    const isLoggedIn = !!req.auth;
-    const isAuthPage = req.nextUrl.pathname.startsWith("/login");
-    const isProtected = req.nextUrl.pathname.startsWith("/dashboard");
-    
-    if (isProtected && !isLoggedIn) {
-      return NextResponse.redirect(new URL("/login", req.nextUrl));
-    }
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const isAuthPage = req.nextUrl.pathname.startsWith("/login");
+  const isProtected = req.nextUrl.pathname.startsWith("/dashboard");
+  const isHome = req.nextUrl.pathname === "/";
 
-    if (isAuthPage && isLoggedIn) {
-      return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
-    }
+  if (isHome) {
+    return NextResponse.redirect(
+      new URL(isLoggedIn ? "/dashboard" : "/login", req.nextUrl)
+    );
+  }
 
-    return NextResponse.next();
-  });
+  if (isProtected && !isLoggedIn) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
 
-  export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)"],
-  };
+  if (isAuthPage && isLoggedIn) {
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    "/",                   
+    "/dashboard/:path*",
+  ]
+};
